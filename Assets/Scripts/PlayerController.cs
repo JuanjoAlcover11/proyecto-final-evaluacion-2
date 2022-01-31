cca1794df3652cfe5642ab0db9287750a9d64a28
JuanjoAlcover11 <juanjoalcover2001@gmail.com>
2022-01-31 10:39:08 +0100

    1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float velocidad = 8f;
+    private float turnSpeed = 100f;
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    private Vector3 offset = new Vector3(0, 0, 0);
+    void Start()
+    {
+        transform.position = offset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * velocidad * Time.deltaTime * verticalInput);
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+
+
+    }
+}
---
+++ Assets/Scripts/RotateTower.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTower : MonoBehaviour
+{
+    private float rotationSpeed = 200f;
+    void Update()
+    {
+        //float horizontalInputMouse = Input.GetAxis("Mouse X");
+        //transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime * horizontalInputMouse);
+    }
+}
